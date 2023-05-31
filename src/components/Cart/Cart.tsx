3c15a9e2cdde1840b637a676e244f51adb3ad893
pawgog <pgogola10@gmail.com>
2023-05-31 22:05:20 +0200

    Add spinner to cart component

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -3,12 +3,25 @@ import * as S from './Cart.styled';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLeftLong } from '@fortawesome/free-solid-svg-icons';
 import { UrlContext } from '../../utils/context';
+import { staticText } from '../../utils/staticText';
 import useFetchingCart from '../../hooks/useFetchingCart';
+import Spinner from '../../globalComponent/Spinner';
+import Error from '../../globalComponent/Error';
 
 const Cart = () => {
   const url = useContext(UrlContext);
   const apiUrlCart = `${url}/cart`;
-  const { cartItems, pricesSum } = useFetchingCart(apiUrlCart);
+  const { cartItems, pricesSum, isLoading, isError } = useFetchingCart(apiUrlCart);
+
+  if (isLoading) {
+    <Spinner />
+  }
+  if (isError && !isLoading)
+  return (
+    <Error>
+      <h2>{staticText.error}</h2>
+    </Error>
+  );
 
   return (
     <>
@@ -17,31 +30,37 @@ const Cart = () => {
         <FontAwesomeIcon icon={faLeftLong} />
       </S.ButtonBack>
       <S.CartBoard>
-        <S.SingleProduct>
-          <table>
-            <thead>
-              <tr>
-                <th>Item</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              {cartItems.map(({ title, quantity=1, prices }) => {
-                return (
-                  <tr key={title}>
-                    <td><img src="https://via.placeholder.com/160" alt={title} /> {title}</td>
-                    <td>{prices[0]?.amount}</td>
-                    <td>{quantity}</td>
-                    <td>{prices[0]?.amount * quantity}</td>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <S.SingleProduct>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Item</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
                   </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </S.SingleProduct>
-        <p>{pricesSum[0]?.amount}</p>    
+                </thead>
+                <tbody>
+                  {cartItems.map(({ title, quantity=1, prices }) => {
+                    return (
+                      <tr key={title}>
+                        <td><img src="https://via.placeholder.com/160" alt={title} /> {title}</td>
+                        <td>{prices[0]?.amount}</td>
+                        <td>{quantity}</td>
+                        <td>{prices[0]?.amount * quantity}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </S.SingleProduct>
+            <p>{pricesSum[0]?.amount}</p>   
+          </>
+        )}
       </S.CartBoard>
     </>
   );
