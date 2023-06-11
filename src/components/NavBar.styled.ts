867cc6963030a31484a9396b9c60d0ba8344e3ba
pawgog <pgogola10@gmail.com>
2023-06-11 18:17:25 +0200

    Add basic style

--- src/components/NavBar.styled.ts
+++ src/components/NavBar.styled.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
+import { colors } from '../globalStyle/colors';
 
 export const NavBar = styled.nav`
   display: flex;
   align-items: center;
-  flex-direction: column;
-  min-width: 100%;
-  min-height: 100%;
+  justify-content: space-between;
+  padding: 2rem;
+  background-color: ${colors.grey};
+  color: ${colors.white};
 `;
