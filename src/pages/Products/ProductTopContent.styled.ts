64f5fbf9cc2fb59eec8eb8e7c8b388759dfdcce7
pawgog <pgogola10@gmail.com>
2023-07-14 18:38:12 +0200

    Flex style on top product page

--- src/pages/Products/ProductTopContent.styled.ts
+++ src/pages/Products/ProductTopContent.styled.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { colors } from '../../globalStyle/colors';
 
 export const ProductContent = styled.div`
+  display: flex;
   width: 100%;
   min-height: 300px;
   color: ${colors.white};
